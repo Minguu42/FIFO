8d4b992c5a0e30d7dfe8916d4a2abf76d63e9673
minguu <minguu42@gmail.com>
2020-10-18 02:08:31 +0900

    検索機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'hackathons#index'
+  get 'hackathons/search', to: 'hackathons#search'
   devise_for :users
   resources :reviews, only: [:index, :destroy]
   resources :hackathons do
