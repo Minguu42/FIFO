2918da5b7716e49d572da6692c1584ad04feab83
minguu <minguu42@gmail.com>
2020-10-18 01:35:32 +0900

    初心者おすすめマークの作成

--- app/helpers/hackathons_helper.rb
+++ app/helpers/hackathons_helper.rb
@@ -1,2 +1,16 @@
 module HackathonsHelper
+
+  def calculate_level_average(hackathon)
+    if hackathon.reviews.any?
+      num_of_level = 0
+      sum_of_level = 0
+      hackathon.reviews.each do |review|
+        sum_of_level += review.level
+        num_of_level += 1
+      end
+      sum_of_level / num_of_level
+    else
+      0
+    end
+  end
 end
