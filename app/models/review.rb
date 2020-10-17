65c7bce046b377dc25b5300fe01f9ebfe45eef16
minguu <minguu42@gmail.com>
2020-10-18 07:19:38 +0900

    リサイズした画像を表示

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,12 @@ class Review < ApplicationRecord
   validates :role, presence: true, length: {maximum: 200}
   validates :level, presence: true
   validates :comment, presence: true
+  validates :image, content_type: {in: %w[image/jpeg image/gif image/png],
+                                   message: "must be a valid image format"},
+            size: {less_than: 5.megabytes,
+                   message: "should be less than 5MB"}
+
+  def display_image
+    image.variant(resize_to_limit: [275, 275])
+  end
 end
