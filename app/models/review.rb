bd3126857422604daf9c52a9e9fa5e294c713915
minguu <minguu42@gmail.com>
2020-10-18 07:08:48 +0900

    画像アップロード機能の追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :hackathon
+  has_one_attached :image
 
   validates :title, presence: true, length: {maximum: 200}
   validates :role, presence: true, length: {maximum: 200}
