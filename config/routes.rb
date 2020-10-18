ba0f21f539937ca483132e5a6930814c124f2ce7
minguu <minguu42@gmail.com>
2020-10-18 13:34:43 +0900

    ボタンなどのUIを調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'hackathons#index'
   get 'hackathons/search', to: 'hackathons#search'
-  get 'cheer', to: 'hackathons#cheer'
   devise_for :users
   resources :reviews, only: [:index, :destroy]
   resources :hackathons do
